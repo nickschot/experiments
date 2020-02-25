ad0b82750d64c5ad2fef7d47fe095e90010c7067
Nick Schot <nickschot@gmail.com>
2020-02-25 21:20:36 +0100

    configure for github pages

--- config/environment.js
+++ config/environment.js
@@ -45,6 +45,8 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
+    ENV.rootURL = '/experiments';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
